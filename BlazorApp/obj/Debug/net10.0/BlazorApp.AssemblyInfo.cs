5acfe7691188b40fec7fccb9554f85d505b1e61b
Haseeb Tahir <125865207+Developer566@users.noreply.github.com>
2026-02-24 10:19:26 +0500

    registr form with toaster service

--- BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa5d63e248fe8b22d6c55082953c815b17a010c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
