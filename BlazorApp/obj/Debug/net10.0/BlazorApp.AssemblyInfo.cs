c40d16930b1cc4d9b407edfd4e986ece11181020
Haseeb Tahir <125865207+Developer566@users.noreply.github.com>
2026-02-26 13:27:39 +0500

    Dashboards redesign

--- BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+605e26dd2cd233f1381fa7b4b7be09c7ccd67a42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c4756202a195f3bee0db823d96dffe956973d52")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
